d5959ac414b91647a4f6d755b4d5271644732e29
dityak <dityasiddaraj@gmail.com>
2025-07-22 10:18:40 +0530

    Fix deployment crash on Render

--- server/index.js
+++ server/index.js
@@ -70,7 +70,7 @@ app.get('/api/weather/:city', async (req, res) => {
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
